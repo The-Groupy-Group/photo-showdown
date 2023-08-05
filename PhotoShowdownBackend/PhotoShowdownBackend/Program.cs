751263f39ba2347d063df8592db06da118e012f4
IdoRoss <69646826+IdoRoss@users.noreply.github.com>
2023-08-05 22:14:17 +0300

    Create DB Context with users table connected in memory

--- PhotoShowdownBackend/PhotoShowdownBackend/Program.cs
+++ PhotoShowdownBackend/PhotoShowdownBackend/Program.cs
@@ -1,6 +1,18 @@
+global using PhotoShowdownBackend.Models;
+global using PhotoShowdownBackend.Data;
+global using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<PhotoShowdownDbContext>(options =>
+    {
+        options.UseInMemoryDatabase("PhotoShowdownDB");
+
+        // In due time amen
+        //options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    }
+);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
