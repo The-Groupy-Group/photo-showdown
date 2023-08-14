c0d0def5d11ab405a658650b198be7e6fea95c39
Ziv Siag <ziv.siag@gmail.com>
2023-08-14 18:16:49 +0300

    login screen+service+token

---
+++ photo-showdown-frontend/src/app/users/components/user-login/user-login.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import {UsersService } from '../../services/users/users.service'
+import { HttpErrorResponse } from '@angular/common/http';
+import { AuthorizationService } from '../../services/authorization/authorization.service';
+@Component({
+  selector: 'app-user-login',
+  templateUrl: './user-login.component.html',
+  styleUrls: ['./user-login.component.css']
+})
+export class UserLoginComponent
+{
+  username:string='';
+  password:string='';
+  errorMessage?:string;
+  constructor(private readonly authorizationService: AuthorizationService) {}
+   onSubmit(form:NgForm)
+  {
+      this.username=form.value.username;
+      this.password=form.value.password;
+
+      console.log(this.username)
+      console.log(this.password)
+      this.authorizationService.login(this.username,this.password).subscribe({
+        next:(response)=>
+        {
+          this.errorMessage=undefined;
+          console.log(response);
+        },
+        error:((error:HttpErrorResponse)=>
+        {
+          this.errorMessage=error.error.message;
+        })
+
+
+      })
+
+
+  }
+}
