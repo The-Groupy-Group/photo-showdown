fb073217b7ec44d6e74ff987a252da87e3bc2012
Ziv Siag <ziv.siag@gmail.com>
2023-08-16 14:49:27 +0300

    create router

--- photo-showdown-frontend/src/app/app-routing.module.ts
+++ photo-showdown-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserLoginComponent } from './users/components/user-login/user-login.component';
+import { UserRegistrationComponent } from './users/components/user-registration/user-registration.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component:UserLoginComponent,
+    path:'login'
+  },
+  {
+    component:UserRegistrationComponent,
+    path:'register'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
