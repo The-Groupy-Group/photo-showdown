8eb0376f24fb2fdc738632d78e4a56a20bea819c
Nadavrav <nadavrav@post.bgu.ac.il>
2023-09-01 13:50:03 +0300

    Create Matches

---
+++ PhotoShowdownBackend/PhotoShowdownBackend/Repositories/Matches/IMatchesReporitory.cs
@@ -0,0 +1,11 @@
+﻿using PhotoShowdownBackend.Models;
+using PhotoShowdownBackend.Repositories.Repository;
+
+namespace PhotoShowdownBackend.Repositories.Users;
+
+/// <summary>
+/// Data Access Layer (DAL) interface for our Users
+/// </summary>
+public interface IMatchesReporitory : IRepository<Match>
+{
+}
---
+++ SQL/5_CreateMatchesTable.sql
@@ -0,0 +1,16 @@
+﻿USE PhotoShowdownDB;
+GO
+
+-- Create the Matches table
+CREATE TABLE Matches (
+    Id INT PRIMARY KEY IDENTITY,
+    OwnerId INT NOT NULL,
+    StartDate DATE,
+	EndDate DATE,
+	CONSTRAINT FK_Matches_Users FOREIGN KEY (OwnerId) REFERENCES Users(Id)
+);
+GO 
+ALTER TABLE Users
+ADD MatchId INT,
+CONSTRAINT FK_Users_Matches FOREIGN KEY (MatchId) REFERENCES Matches(Id);
+GO
