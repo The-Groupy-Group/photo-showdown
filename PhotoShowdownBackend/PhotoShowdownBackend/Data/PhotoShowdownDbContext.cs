14502583e5ddb6d31f99fd6f35642592510e8be9
IdoRoss <69646826+IdoRoss@users.noreply.github.com>
2024-01-16 17:45:45 +0200

    Implemented migration manager

--- PhotoShowdownBackend/PhotoShowdownBackend/Data/PhotoShowdownDbContext.cs
+++ PhotoShowdownBackend/PhotoShowdownBackend/Data/PhotoShowdownDbContext.cs
@@ -6,15 +6,12 @@ namespace PhotoShowdownBackend.Data;
 /// <summary>
 /// The DbContext for our application
 /// </summary>
-public class PhotoShowdownDbContext: DbContext
+public class PhotoShowdownDbContext : DbContext
 {
-    public PhotoShowdownDbContext(DbContextOptions<PhotoShowdownDbContext> options) : base(options)
-    {
-    }
+    public PhotoShowdownDbContext(DbContextOptions<PhotoShowdownDbContext> options) : base(options) { }
 
     public DbSet<User> Users { get; set; } = null!;
     public DbSet<Picture> Pictures { get; set; } = null!;
     public DbSet<Match> Matches { get; set; } = null!;
     public DbSet<MatchConnection> MatchConnections { get; set; } = null!;
-
 }
