6f7c146f40ea146de82d85cdb65453a8a3443019
IdoRoss <69646826+IdoRoss@users.noreply.github.com>
2024-01-18 14:55:51 +0200

    Migration manager split commands in a script and refactors

--- PhotoShowdownBackend/PhotoShowdownBackend/Utils/SQLServerMigrationExecutor.cs
+++ PhotoShowdownBackend/PhotoShowdownBackend/Utils/SQLServerMigrationExecutor.cs
@@ -24,31 +24,53 @@ public class SQLServerMigrationExecutor
     public void ExecutePendingScripts()
     {
         _logger.LogInformation($"{nameof(ExecutePendingScripts)} Start");
-        using var connection = new SqlConnection(connectionString);
+        using SqlConnection connection = new(connectionString);
         connection.Open();
 
-        var dbScriptsTableExists = TableExists(connection, "DBScripts");
+        bool dbScriptsTableExists = TableExists(connection, "DBScripts");
 
         if (!dbScriptsTableExists)
         {
-            // If the DBScripts table doesnt exist, No migration was ever tracked
-            // Create the DBScripts table
             _logger.LogInformation("DBScripts folder doesnt exist. Creating it.");
             CreateDBScriptsTable(connection);
 
-            var numOfTables = NumOfTables(connection);
+            int numOfTables = NumOfTables(connection);
             if (numOfTables > 1)
             {
-                // Handle edge case where the db was already build but no migration was tracked
-                // Add all existing SQL files to DBScripts table without executing them (Need to make sure they were all executed manually)
-                _logger.LogInformation("DB was already build but no migration was ever tracked, Adding all scripts to DBScripts without executing");
-                AddScriptsToMigration(connection);
+                _logger.LogInformation("DB was already build but no migration was ever tracked, Adding all scripts to DBScripts without executing.");
+
+                IterateAllScripts(connection, execute: false);
+                return;
             }
         }
-
-        // Execute all scripts that were never executed before
         _logger.LogInformation("Looking for scripts that were never executed before.");
-        ExecuteAndAddScriptsToMigration(connection);
+        IterateAllScripts(connection, execute:true);
+    }
+
+    private void IterateAllScripts(SqlConnection connection, bool execute)
+    {
+        var scriptFiles = Directory.GetFiles(SystemSettings.SQLScriptsFolderName, "*.sql");
+        Array.Sort(scriptFiles, (x, y) => GetScriptBatchFromFileName(x).CompareTo(GetScriptBatchFromFileName(y)));
+
+        foreach (var scriptFile in scriptFiles)
+        {
+            int scriptBatch = GetScriptBatchFromFileName(scriptFile);
+            string scriptName = Path.GetFileName(scriptFile);
+
+            // Check if the script is already in DBScripts table
+            if (!IsScriptInDBScriptsTable(connection, scriptBatch))
+            {
+                if (execute)
+                {
+                    _logger.LogInformation($"Script {scriptName} has never been executed. Executing it.");
+
+                    ExecuteScript(connection, scriptFile);
+                }
+                _logger.LogInformation($"Adding script {scriptName} to DBScripts table.");
+                // Add script to DBScripts table
+                AddScriptToDBScriptsTable(connection, scriptBatch, scriptName);
+            }
+        }
     }
 
     private static bool TableExists(SqlConnection connection, string tableName)
@@ -79,48 +101,6 @@ public class SQLServerMigrationExecutor
         command.ExecuteNonQuery();
     }
 
-    private void AddScriptsToMigration(SqlConnection connection)
-    {
-        var scriptFiles = Directory.GetFiles(SystemSettings.SQLScriptsFolderName, "*.sql")
-            .OrderBy(GetScriptBatchFromFileName);
-
-        foreach (var scriptFile in scriptFiles)
-        {
-            var scriptBatch = GetScriptBatchFromFileName(scriptFile);
-            var scriptName = Path.GetFileName(scriptFile);
-
-            _logger.LogInformation($"Adding script {scriptName} to DBScripts table as if it was executed.");
-
-            // Add script to DBScripts table
-            AddScriptToDBScriptsTable(connection, scriptBatch, scriptName);
-        }
-    }
-
-    private void ExecuteAndAddScriptsToMigration(SqlConnection connection)
-    {
-        var scriptFiles = Directory.GetFiles(SystemSettings.SQLScriptsFolderName, "*.sql");
-
-        // Sort script files by their ScriptBatch
-        var sortedScriptFiles = scriptFiles.OrderBy(GetScriptBatchFromFileName);
-
-        foreach (var scriptFile in sortedScriptFiles)
-        {
-            var scriptBatch = GetScriptBatchFromFileName(scriptFile);
-            var scriptName = Path.GetFileName(scriptFile);
-
-            // Check if the script is already in DBScripts table
-            if (!IsScriptInDBScriptsTable(connection, scriptBatch))
-            {
-                _logger.LogInformation($"Script name {scriptName} has never been executed. Executing it now and adding to DBScripts table.");
-                // Execute the script
-                ExecuteScript(connection, scriptFile);
-
-                // Add script to DBScripts table
-                AddScriptToDBScriptsTable(connection, scriptBatch, scriptName);
-            }
-        }
-    }
-
     private static int GetScriptBatchFromFileName(string fileName)
     {
         // Extract the script number from the file name
@@ -140,10 +120,16 @@ public class SQLServerMigrationExecutor
 
     private static void ExecuteScript(SqlConnection connection, string scriptPath)
     {
-        // Read the script file and execute it
-        var script = File.ReadAllText(scriptPath);
-        using var command = new SqlCommand(script, connection);
-        command.ExecuteNonQuery();
+        // Read the script file
+        string script = File.ReadAllText(scriptPath);
+        // Split the script into commands
+        string[] commands = script.Split("GO", StringSplitOptions.RemoveEmptyEntries);
+        // Execute each command
+        foreach (string command in commands)
+        {
+            using var sqlCommand = new SqlCommand(command, connection);
+            sqlCommand.ExecuteNonQuery();
+        }
     }
 
     private static void AddScriptToDBScriptsTable(SqlConnection connection, int scriptBatch, string scriptName)
