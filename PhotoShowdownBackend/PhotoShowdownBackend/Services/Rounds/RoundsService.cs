c9575a9d80523b01c32c9bb35502c52ae8a07679
Nadavrav <nadavrav@post.bgu.ac.il>
2024-02-16 16:06:09 +0200

    RoundsService constructor

--- PhotoShowdownBackend/PhotoShowdownBackend/Services/Rounds/RoundsService.cs
+++ PhotoShowdownBackend/PhotoShowdownBackend/Services/Rounds/RoundsService.cs
@@ -1,5 +1,21 @@
-﻿namespace PhotoShowdownBackend.Services.Rounds;
+﻿using AutoMapper;
+using PhotoShowdownBackend.Repositories.Pictures;
+using PhotoShowdownBackend.Repositories.Rounds;
+using PhotoShowdownBackend.Services.Pictures;
+
+namespace PhotoShowdownBackend.Services.Rounds;
 
 public class RoundsService: IRoundsService
 {
+    private readonly IRoundsRepository _roundsRepo;
+    private readonly IMapper _mapper;
+    private readonly ILogger<RoundsService> _logger;
+
+    public RoundsService(IRoundsRepository roundsRepo,IMapper mapper,ILogger<RoundsService> logger)
+    {
+        _roundsRepo = roundsRepo;
+        _mapper = mapper;
+        _logger = logger;
+    }
+
 }
