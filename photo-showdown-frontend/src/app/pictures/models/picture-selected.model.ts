d405d8dfbbe8dff172bc363fc3b4cab6f6f1aebd
IdoRoss <69646826+IdoRoss@users.noreply.github.com>
2024-02-26 21:45:10 +0200

    Design match list

--- photo-showdown-frontend/src/app/pictures/models/picture-selected.model.ts
+++ photo-showdown-frontend/src/app/pictures/models/picture-selected.model.ts
@@ -1,7 +1,9 @@
 import { UserPublicDetails } from 'src/app/users/models/user-public-details.model';
 import { Picture } from './picture.model';
 
-export interface PictureSelected extends Picture {
+export interface PictureSelected{
+  id: number;
+  picturePath: string;
   numOfVotes: number;
   selectedByUser?: UserPublicDetails;
   usersVoted: UserPublicDetails[];
