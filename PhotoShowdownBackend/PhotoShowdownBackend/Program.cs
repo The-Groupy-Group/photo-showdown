0eadec187b7c2034dd9949052e72c386889d07f5
IdoRoss <69646826+IdoRoss@users.noreply.github.com>
2024-03-01 11:51:25 +0200

    v0.5.0

--- PhotoShowdownBackend/PhotoShowdownBackend/Program.cs
+++ PhotoShowdownBackend/PhotoShowdownBackend/Program.cs
@@ -145,7 +145,7 @@ builder.Services.AddSwaggerGen(options =>
             new OpenApiInfo
             {
                 Title = "Donfil API",
-                Version = "0.4.0"
+                Version = "0.5.0"
             });
 
     // Add JWT support for swagger
