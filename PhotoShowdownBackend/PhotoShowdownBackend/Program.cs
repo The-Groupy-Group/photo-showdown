831f91a53b53378f778dc10c08164235e16c1d55
IdoRoss <69646826+IdoRoss@users.noreply.github.com>
2024-03-08 12:32:51 +0200

    Add prod cors policy to api

--- PhotoShowdownBackend/PhotoShowdownBackend/Program.cs
+++ PhotoShowdownBackend/PhotoShowdownBackend/Program.cs
@@ -127,11 +127,20 @@ builder.Services.AddAutoMapper(typeof(MappingConfig).Assembly);
 builder.Services.AddHttpContextAccessor();
 
 // Add Cors
+const string CORS_DEV_POLICY = "CorsDevPolicy";
+const string CORS_PROD_POLICY = "CorsProdPolicy";
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("CorsPolicy", build =>
+    options.AddPolicy(CORS_DEV_POLICY, build =>
     {
         build.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+    });
+
+    options.AddPolicy(CORS_PROD_POLICY, build =>
+    {
+        build.WithOrigins("https://photo-showdown.vercel.app")
+            .AllowAnyMethod()
+            .AllowAnyHeader();
     });
 });
 
@@ -213,7 +222,7 @@ app.UseStaticFiles(new StaticFileOptions
 });
 
 // Use Cors
-app.UseCors("CorsPolicy");
+app.UseCors(app.Environment.IsDevelopment() ? CORS_DEV_POLICY : CORS_PROD_POLICY);
 
 app.UseHttpsRedirection();
 
