9991c9e3e469636fce4d3eade7c5db5729db4b3e
Nadavrav <nadavrav@post.bgu.ac.il>
2025-12-13 17:39:24 +0200

    addresses update

--- PhotoShowdownMobile/src/utils/Config.ts
+++ PhotoShowdownMobile/src/utils/Config.ts
@@ -1,8 +1,9 @@
+// src/utils/Config.ts
 
-const IP_ADDRESS = "192.168.1.15"; 
+const NGROK_URL = "https://transcendentalistic-ardith-couth.ngrok-free.dev"; 
 
-const PORT = "5299";
+export const API_BASE = `${NGROK_URL}/api`;
 
-export const API_BASE = `http://${IP_ADDRESS}:${PORT}/api`;
-export const WS_URL = `ws://${IP_ADDRESS}:${PORT}/api/ws`;
-export const IMAGE_BASE = `http://${IP_ADDRESS}:${PORT}`;
+export const WS_URL = `${NGROK_URL.replace('https', 'wss')}/api/ws`;
+
+export const IMAGE_BASE = NGROK_URL;
